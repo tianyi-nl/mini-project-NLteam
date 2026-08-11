823d744840e5385543bca54389fbd9d42db45d84
Tianyi Xue <tianyixue@MacBookPro.home>
2026-08-11 19:39:06 +0200

    Day 1 DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import "./index.css"
 
 function App() {
-  return 
-  <>
-  
-  </>;
+  return (
+    <div className="app-layout">
+      <Navbar />
+      <div className="app-body">
+        <Sidebar />
+        <main className="app-content">
+          <p>main page will go here </p>
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
-function Navbar (){
+import logo from "../assets/logo.png";
+import "./Navbar.css"
 
-    return
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="Recipe Book Logo" className="navbar-logo"/>
+      <h1>Recipe Book</h1>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
